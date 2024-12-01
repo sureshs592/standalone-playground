327b343d748abb5b95d91f98a90eb3b40c130771
Suresh Subramaniam <sureshs.592@gmail.com>
2024-12-01 13:36:23 +0000

    Zendesk playground setup

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@ import playground from './playground.html';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response(playground, {
-			headers: {
-				'content-type': 'text/html',
-			},
-		});
+		let url = new URL(request.url);
+
+		if (url.pathname.startsWith('/zendesk/')) {
+			return new Response(playground, {
+				headers: {
+					'content-type': 'text/html',
+				},
+			});
+		} else {
+			return new Response('Supported URL: /zendesk/{subdomain}');
+		}
 	},
 } satisfies ExportedHandler<Env>;
